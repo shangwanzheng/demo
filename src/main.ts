d6a132f9ffcf97ec79aa8ecae7a685b82e2c0783
swz <417888981@qq.com>
2025-10-13 16:45:48 +0800

    clean code

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,11 @@ import './assets/base.css'
 import './assets/all.min.js'
 // import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 // import ElementPlus from 'element-plus'
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
 // app.use(ElementPlus)
-app.use(router)
 
 app.mount('#app')
